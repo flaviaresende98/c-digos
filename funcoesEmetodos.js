5d2f206e2742a61ba87d2d9e5234c195413d5868
flaviaresende98@gmail.com <flavinha98@sempreceub.com>
2020-06-23 14:21:42 -0300

    Inserindo código com erro funcoes e metodos no JavaScript

--- funcoesEmetodos.js
+++ funcoesEmetodos.js
@@ -1,62 +1,85 @@
-const alunosTurmaA = [
+const classA = [// array de class A
     {
-        nome: "Flávia",
-        nota: 7.5
+        name: "Flávia",
+        grade: 7.5
     },
     {
-        nome: "Daniel",
-        nota: 3.2
+        name: "Daniel",
+        grade: 3.2
     },
     {
-        nome: "Maria Clara",
-        nota: 7.9
+        name: "Maria Clara",
+        grade: 7.9
     },
     {
-        nome: 'Daniel',
-        nota: 1.2
+        name: 'Daniel',
+        grade: 1.2
     }
 ]
-const alunosTurmaB = [
+const classB = [// array de class B
     {
-        nome: "Eliane",
-        nota: 8.8
+        name: "Eliane",
+        grade: 8.8
     },
     {
-        nome: "Sérgio",
-        nota: 7.4
+        name: "Sérgio",
+        grade: 7.4
     },
     {
-        nome: "José",
-        nota: 6.9
+        name: "José",
+        grade: 6.9
     },
     {
-        nome : "Novo Aluno",
-        nota : 5
+        name: "Novo student",
+        grade: 5
     }
 ]
-function calculoMedia(alunos) {
-    let soma = 0;
-    for (let i = 0; i < alunos.length ; i++){
-            soma = soma +alunos[i].nota
-
+function calculateAvarage(students) {
+    let sum = 0;
+    for (let i = 0; i < students.length; i++) {
+        sum = sum + students[i].grade
 
     }
-    const media = soma / alunos.length
-    return media
+    const avarage = sum / students.length
+    return avarage
 }
 
-const media1 = calculoMedia(alunosTurmaA)
-const media2 = calculoMedia(alunosTurmaB)
 
-
-function enviaMensagem(media, turma) {
-    if (media > 5){
-        console.log(`A media da ${turma} foi de ${media}. Parabéns`)
-    }else {
-        console.log(`A média da ${turma} é menor que 5`)
+function sendMessage(avarage, studentsClass) {
+    if (avarage > 5) {
+        console.log(` ${studentsClass} avarage: ${avarage}. Congratulations!`)
+    } else {
+        console.log(` ${studentsClass} avarage: ${avarage} is not good enough!`)
     }
 }
 
-enviaMensagem(media1, `Turma A`)
-enviaMensagem(media2, `Turma B`)
 
+function markAsFlunked(student) {
+    student.flunked = false;
+    if (student.grade < 5) {
+        student.flunked = true;
+    }
+}
+
+function sendMessageflunked(student) {
+    if (student.flunked) {
+        console.log(`O student ${student.name} flunked!`)
+    }
+}
+
+function studentsflunkeds(students) {
+    for (let student of students) {
+        markAsFlunked(student)
+        sendMessageflunkeds(student)
+    }
+}
+
+const avarage1 = calculateAvarage(classA)
+const avarage2 = calculateAvarage(classB)
+
+
+sendMessage(avarage1, `class A`)
+sendMessage(avarage2,`class B`)
+
+sendMessageflunked(classA)
+sendMessageflunked(classB)
